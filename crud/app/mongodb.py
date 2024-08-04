37868dfbb3a9718b04566316f2bf1694f1499214
Atique Ahmad <>
2024-08-04 18:03:42 +0500

    Clean up main branch

--- crud/app/mongodb.py
+++
@@ -1,8 +0,0 @@
-from crud.settings import MONGODB_URI
-from pymongo import MongoClient
-
-client = MongoClient(MONGODB_URI)
-db = client["crudappdb"]
-
-def get_collection(collection_name):
-    return db[collection_name]
